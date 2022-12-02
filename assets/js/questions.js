6bcbb8881e5937410f746aa041f0fc66de710366
TamzinC <115940593+TamzinC@users.noreply.github.com>
2022-12-02 22:34:29 +0000

    updated questions.js to add all the questions for the quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title : 'How many arguments are passed into a function?',
+        choices : ['Three', 'One', 'Two', 'As many as you like'],
+        answer : 'Two'
+    }
+    {
+        title : 'What bracket styles are used for objects?',
+        choices : ['Curly brackets', 'Square brackets', 'Parentheses', 'Angle brackets'],
+        answer : 'Curly brackets'
+    }
+    {
+        title : 'What selector would you use to grab a specific element that matches a specified CSS selector?',
+        choices : ['getElementbyId', 'querySelectorAll', 'querySelector', 'getElementsByTagName'],
+        answer : 'querySelector'
+    }
+    {
+        title : 'What does the event listener listen out for?',
+        choices : ['Click', 'Keyboard', 'Mousemove', 'All of the above']
+        answer : 'All of the above'
+    }
+    {
+        title : 'DRY is an abbreviation of what?',
+        choices : ['Don\'t Repeat Yourself!', 'Donuts R Yum!', 'Drive Round Yachts!', 'Do Right Yo-Yo\'s!'],
+        answer : 'Don\'t Repeat Yourself!'
+    }
+];
