e08b03db1ef48eeb241409e532c64a093da4a1c0
TamzinC <115940593+TamzinC@users.noreply.github.com>
2022-12-09 00:07:54 +0000

    updated localStorage code to save the scores and initials and added link to scores.js file into the index.html file as this was missing

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -3,23 +3,24 @@
 var finalScore = document.querySelector('#final-score');
 var initials = document.querySelector('#initials');
 var submit = document.querySelector('#submit');
-
-//Saving the scores as an object of the score and initials element
-var highscores = {
-    score: document.getElementById('#final-score'),
-    initials: document.getElementById('#initials')
-}
-
-
+var highscoresEl = document.querySelector('#highscores');
 
 
 //creating an event listener for the submit button
 submit.addEventListener('click', function () {
+    //Saving the scores as an object of the score and initials element
+    var highscores = {
+        score: time,
+        initials: initials.value
+    }
+    console.log(highscores);
     //saving the score and initials
     localStorage.setItem('score', JSON.stringify(highscores));
-    // localStorage.setItem(['score', JSON.stringify(finalScore)], ['initials', JSON.stringify(initials)]);
+    
     
     //this needs to be displayed upon load of highscores html page - separate event listener for this page?
     var scoreboard = document.createElement('li');
     scoreboard.innerText = JSON.parse(localStorage.getItem(['score'], ['initials']));
+    // choices.append(choicesButton);
+    scoreboard.append(highscoresEl);
 })
